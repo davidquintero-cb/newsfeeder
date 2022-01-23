7881dd06f421a45da060b279a2a6b29b38821347
David Quintero <davidquintero@Davids-MBP.lan>
2022-01-22 21:24:38 -0500

    configure testing package

--- httpd/main.go
+++ httpd/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"newsfeeder/httpd/handler"
-
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"newsfeeder/platform/newsfeed"
 )
 
 func main() {
-	r := gin.Default()
+	// r := gin.Default()
 
-	r.GET("/ping", handler.PingGet())
+	// r.GET("/ping", handler.PingGet())
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	feed := newsfeed.New()
+
+	fmt.Println(feed)
+
+	feed.Add(newsfeed.Item{"Hello", "How are you?"})
+
+	fmt.Println(feed)
 }
