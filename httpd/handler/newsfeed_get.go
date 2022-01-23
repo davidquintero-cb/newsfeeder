4ae3286533e46d8fadacf5792fc4396f56a40bc2
David Quintero <davidquintero@Davids-MBP.lan>
2022-01-22 23:16:28 -0500

    creater Getter and Adder interfaces

--- httpd/handler/newsfeed_get.go
+++ httpd/handler/newsfeed_get.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func NewsfeedGet(feed *newsfeed.Repo) gin.HandlerFunc {
+func NewsfeedGet(feed newsfeed.Getter) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		result := feed.GetAll()
 		c.JSON(http.StatusOK, result)
